4a3a251b2b0ee6b2d161f0230f5e081bd5eeadfb
Kirby T. Wu <tkirby@gmail.com>
2013-02-08 18:44:17 +0800

    add viewer sample

--- index.js
+++ index.js
@@ -1,1 +1,99 @@
-$(document).ready(function(){});
+var slice$ = [].slice;
+$(document).ready(function(){
+  var px, twstat, reformat, update;
+  px = null;
+  twstat = (function(){
+    twstat.displayName = 'twstat';
+    var prototype = twstat.prototype, constructor = twstat;
+    function twstat(data){
+      this.px = new Px(data);
+      this.data = this.px.data;
+      console.log(this.px);
+      this.index = ["數據指標"].concat(this.px.metadata.VALUES["指標"].map(function(it){
+        return it.trim();
+      }));
+      this.year = ["數據年份"].concat(this.px.metadata.VALUES["期間"]);
+      this.county = this.px.metadata.VALUES["縣市"];
+      this.tableSize = (this.year.length - 1) * this.county.length;
+    }
+    return twstat;
+  }());
+  reformat = function(v){
+    var u;
+    u = parseFloat(v);
+    if (isNaN(u)) {
+      return "N/A";
+    }
+    if (u < 100) {
+      return parseInt(u * 100) / 100;
+    }
+    u = parseInt(u);
+    if (u > 1000000) {
+      return u / 1000000 + "M";
+    }
+    if (u > 1000) {
+      return u / 1000 + "K";
+    }
+    return u;
+  };
+  update = function(){
+    var index, year, start, end, list, data, res$, i$, ref$, len$, i, n, counties, divs, names, values;
+    index = parseInt($('#index-chooser').val());
+    year = parseInt($('#year-chooser').val());
+    if (index <= 0 || year <= 0) {
+      return;
+    }
+    start = (index - 1) * px.tableSize + px.county.length * (year - 1);
+    end = start + px.county.length;
+    list = slice$.call(px.data, start, end);
+    res$ = [];
+    for (i$ = 0, len$ = (ref$ = px.county).length; i$ < len$; ++i$) {
+      i = i$;
+      n = ref$[i$];
+      res$.push({
+        "name": n,
+        "value": reformat(list[i])
+      });
+    }
+    data = res$;
+    counties = d3.select('#content').selectAll('div.county').data(data);
+    divs = counties.enter().append('div').attr('class', 'county');
+    names = divs.append('div.county').append('div').attr('class', 'name').text(function(it){
+      return it.name + "";
+    });
+    values = divs.append('div').attr('class', 'value').text(function(it){
+      return it.value + "";
+    });
+    counties.select('div.name').text(function(it){
+      return it.name;
+    });
+    return counties.select('div.value').text(function(it){
+      return it.value;
+    });
+  };
+  return d3.json('raw/county/index.json', function(data){
+    var pxIndex, keys, res$, k, v;
+    pxIndex = data[0];
+    res$ = [];
+    for (k in pxIndex) {
+      res$.push(k);
+    }
+    keys = res$;
+    k = keys[parseInt(Math.random() * keys.length)];
+    v = pxIndex[k];
+    d3.select('#index-name').text(v);
+    return $.ajax("raw/county/" + k).done(function(data){
+      px = new twstat(data);
+      d3.select('#index-chooser').on('change', update).selectAll('option').data(px.index).enter().append('option').attr('value', function(d, i){
+        return i;
+      }).text(function(it){
+        return it;
+      });
+      return d3.select('#year-chooser').on('change', update).selectAll('option').data(px.year).enter().append('option').attr('value', function(d, i){
+        return i;
+      }).text(function(it){
+        return it;
+      });
+    });
+  });
+});
--- index.ls
+++ index.ls
@@ -1,2 +1,55 @@
 <- $ document .ready
 
+px = null
+
+class twstat
+  (data) ->
+    @px = new Px data
+    @data = @px.data
+    console.log @px
+    @index = ["數據指標"] +++ @px.metadata.VALUES["指標"].map (-> it.trim!)
+    @year  = ["數據年份"] +++ @px.metadata.VALUES["期間"]
+    @county = @px.metadata.VALUES["縣市"]
+    @table-size = (@year.length - 1) * @county.length
+
+reformat = (v) ->
+  u = parseFloat v
+  if isNaN u then return "N/A"
+  if u<100 then return parseInt(u*100)/100
+  u = parseInt u
+  if u>1000000 then return "#{u/1000000}M"
+  if u>1000 then return "#{u/1000}K"
+  u
+  
+
+update = ->
+  index = parseInt($ \#index-chooser .val!)
+  year  = parseInt($ \#year-chooser .val!)
+  if index<=0 or year<=0 then return
+  start = (index - 1)*px.table-size + px.county.length * (year - 1)
+  end   = start + px.county.length
+  list = px.data[start til end]
+  data = [{"name": n, "value": reformat list[i]} for n,i in px.county]
+  counties = d3.select \#content .selectAll \div.county .data data
+  divs = counties.enter! .append \div .attr \class \county
+  names = divs.append \div.county .append \div .attr \class \name .text -> "#{it.name}"
+  values = divs.append \div .attr \class \value .text -> "#{it.value}"
+  counties.select \div.name .text (it) -> it.name
+  counties.select \div.value .text (it) -> it.value
+
+d3.json \raw/county/index.json, (data) ->
+  px-index = data.0
+  keys = [k for k of px-index]
+  k = keys[parseInt(Math.random! * keys.length)]
+  v = px-index[k]
+  d3.select \#index-name .text v
+  $.ajax "raw/county/#{k}" .done (data) ->
+    px := new twstat data
+    d3.select \#index-chooser .on \change update
+      .selectAll \option .data px.index .enter! .append \option
+      .attr \value (d,i) -> i 
+      .text (->it)
+    d3.select \#year-chooser .on \change update
+      .selectAll \option .data px.year .enter! .append \option
+      .attr \value (d,i) -> i 
+      .text (->it)
